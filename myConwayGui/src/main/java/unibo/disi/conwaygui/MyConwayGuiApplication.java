12203c2871954d888eef0787dcba314e34b081e2
AndreaGrano <andrea.grano@studio.unibo.it>
2025-03-12 11:15:28 +0100

    myConwayGui: minor changes

--- myConwayGui/src/main/java/unibo/disi/conwaygui/MyConwayGuiApplication.java
+++ myConwayGui/src/main/java/unibo/disi/conwaygui/MyConwayGuiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConwayguiApplication {
+public class MyConwayGuiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConwayguiApplication.class, args);
+		SpringApplication.run(MyConwayGuiApplication.class, args);
 	}
 
 }
